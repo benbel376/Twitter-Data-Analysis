70d2b9e7d9c0ce566ed2533f2ae5c7f8041f0cd7
Biniyam Belayneh <biniyambelayneh376@gmail.com>
2022-04-26 15:14:12 +0300

    cleaning code, correcting comments, and handling empty data, Closes #16

--- extract_dataframe.py
+++ extract_dataframe.py
@@ -35,22 +35,34 @@ class TweetDfExtractor:
         self.tweets_list = tweets_list
 
 
-    # a function that extracts the created_at variable and returns a list of date strings
+    
     def find_created_time(self)->list:
+        """
+        a function that extracts the created_at variable and returns a list of date strings.
+        returns a list of creation date and times.
+        """
         created_at = [] # aholds a list of all created time and date
         for items in self.tweets_list:
-            created_at.append(items['created_at'])
+            created_at.append(items.get('created_at', None))
+        
         return created_at
     
-    # a function that extracts the source variable and returns a list of html hyperlink reference strings
     def find_source(self)->list:
+        """
+        a function that extracts the source variable and returns a list of html hyperlink reference strings.
+        returns a list of soruce hyperlink strings.
+        """
         source = [] # a list that holds hyper link references
         for items in self.tweets_list:
             source.append(items['source'])
         return source
     
-    # a function that extracts the text variable and returns a list of tweet strings
+    
     def find_full_text(self)->list:
+        """
+        a function that extracts the text variable and returns a list of tweet strings.
+        returns two lists of original and cleaned text data.
+        """
         cl_text = [] # holds the clean text
         uncl_text = [] # original text
         for items in self.tweets_list:
@@ -60,8 +72,12 @@ class TweetDfExtractor:
         return cl_text, uncl_text
 
 
-    # a function that extracts polarity and subjectivity from the list of tweet strings.
+    
     def find_sentiments(self, text: list)->list:
+        """
+        a function that extracts polarity and subjectivity from the list of tweet strings.
+        returns a two lists of polarity and subjectivity scores.
+        """
         polarity = [] # contains the polarity values from the sentiment analysis.
         self.subjectivity = [] # contains the subjectivity values from the sentiment analysis.
         for items in text:
@@ -71,56 +87,83 @@ class TweetDfExtractor:
         return polarity, self.subjectivity
     
     
-    # a function that extracts authors name.
+    
     def find_screen_name(self)->list:
+        """
+        a function that extracts screen name.
+        returns a list of screen names.
+        """
         screen_name = [] # list of screen names.
         for items in self.tweets_list:
             screen_name.append(items['user']['screen_name'])
         
         return screen_name
 
-    # a function that extracts the language used in the tweet.
     def find_lang(self)->list:
+        """
+        a function that extracts the language used in the tweet. 
+        returns a list of languages.       
+        """        
         lang = [] # list of languages
         for items in self.tweets_list:
-            lang.append(items['lang'])
+            lang.append(items.get('lang', None))
         
         return lang
 
-    # a function that extracts the number of retweets.
+    
     def find_retweet_count(self)->list:
+        """
+        a function that extracts the number of retweets.
+        returns a list of retweet counts.
+        """
         retweet_count = [] # list of number of retweets.
         for items in self.tweets_list:
-            retweet_count.append(items['retweet_count'])
+            retweet_count.append(items.get('retweet_count', None))
         
         return retweet_count
 
-    # a function that extracts the hashtags used in the tweet.
+    
     def find_hashtags(self)->list:
+        """
+        a function that extracts the hashtags used in the tweet.
+        returns a list of hashtags.
+        """
         hashtags = [] # list of hashtags
         for items in self.tweets_list:
-             hashtags.append(items['entities']['hashtags'])
+             hashtags.append(items.get('entities', {}).get('hashtags', None))
         
         return hashtags
 
-    # a function that extracts the number of friends.
+    
     def find_friends_count(self)->list:
+        """
+        a function that extracts the number of friends.
+        returns a list of number of firends.
+        """
         friends_count = [] # list of number of friends.
         for items in self.tweets_list:
             friends_count.append(items['user']['friends_count'])
         
         return friends_count
 
-    # a function that extracts the statuses count in the tweet.
+    
     def find_statuses_count(self)->list:
+        """
+        a function that extracts the statuses count in the tweet.
+        returns a list of number of statuses count.
+        """
         statuses = [] # list of statuses
         for items in self.tweets_list:
             statuses.append(items['user']['statuses_count'])
         
         return statuses
 
-    # a function that extracts the number of friends.
+    
     def find_followers_count(self)->list:
+        """
+        a function that extracts the number of friends.
+        returns a list of number of followers.
+        """
         followers_count = [] # list of number of followers.
         for items in self.tweets_list:
             followers_count.append(items['user']['followers_count'])
@@ -128,39 +171,57 @@ class TweetDfExtractor:
         return followers_count
 
 
-    # a function that extracts the mentions in the tweet.
+    
     def find_mentions(self)->list:
+        """
+        a function that extracts the mentions in the tweet.
+        returns a list of number of mentions.
+        """
         mentions = [] # list of mentions
         for items in self.tweets_list:
-             mentions.append(items['entities']['user_mentions'])
+             mentions.append(items.get('entities', {}).get('user_mentions', None))
         
         return mentions
 
-    # a function that extracts sensitivity status.
+    
     def is_sensitive(self)->list:
+        """
+        a function that extracts sensitivity status.
+        returns list of sensitivity boolean
+        """
         sensitivity = [] # list of sensitivity status.
         for items in self.tweets_list:
-            sensitivity.append(items['possibly_sensitive'])
+            sensitivity.append(items.get('possibly_sensitive', None))
         
         return sensitivity
 
 
-    # a function that extracts the location.
     def find_location(self)->list:
+        """
+        a function that extracts the location.
+        returns list of locations
+        """
         location = [x.get('user', {}).get('location', None) for x in self.tweets_list]
         return location
 
-    # a function that extracts the number of friends.
+    
     def find_favourite_count(self)->list:
-        favourite_count = [] # list of number of followers.
+        """
+        a function that extracts the number of favourties.
+        returns a list of fav counts.
+        """
+        favourite_count = [] # list of number of favourites.
         for items in self.tweets_list:
             favourite_count.append(items['user']['favourites_count'])
         
         return favourite_count
 
-    # a function that inserts the extracted value lists for each variable into a dataframe.       
+          
     def get_tweet_df(self, save=False)->pd.DataFrame:
-        """required column to be generated you should be creative and add more features"""
+        """
+        a function that inserts the extracted value lists for each variable into a dataframe. 
+        returns a dataframe with all extracted columns
+        """
         
         columns = ['created_at', 'source', 'original_text', 'clean_text', 'polarity', 'subjectivity', 'original_author', 'language', 'retweet_count', 'friends_count', 'hashtags', 'statuses', 'followers_count', 'user_mentions', 'favourites_count', 'location']
         created_at = self.find_created_time()
